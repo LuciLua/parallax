4d4c31f126c6d7913a894076fd20f59bd4b3f2d6
LuciLua <luci.lua81@gmail.com>
2021-08-30 21:10:12 -0300

    :pushpin: markScroll

--- js/app.js
+++ js/app.js
@@ -1,33 +1,33 @@
 let hello = document.getElementById('hello')
-let moon = document.getElementById('moon')
 let montain = document.getElementById('montain')
-let road = document.getElementById('road')
-let text = document.getElementById('text')
-let btn = document.getElementById('btn')
-let header = document.querySelector('header')
-
 let texton = document.querySelector('.texton')
-
 let texto = document.querySelector('.texto')
+let mark= document.querySelectorAll('.mark')
 
 window.addEventListener('scroll', function(){
     let value = this.window.scrollY; 
 
-
-    hello.style.fontSize = `${ 40 + value * 0.1}px`
     hello.style.top = 35 + value * 0.02 + '%'
-    hello.style.opacity = 100 + value * -0.3 + '%'
+    // hello.style.opacity = 100 + value * -0.3 + '%'
 
-    montain.style.transform = `rotate(${225 + value * 0.5}deg)`
+    // montain.style.transform = `rotate(${180 + value * 0.5}deg)`
+    // montain.style.left = `${15 + value * 0.01}vw`
+    montain.style.top = `${36 + value * 0.02}%`
+    montain.style.borderRight = `${0 + value * 0.8}px #fafa4588 solid ` 
 
+    
     texto.style.top = 45 + value * 0.04 + '%'
+    texto.style.opacity = 100 - value * 0.4 + '%'
     
     if(value * 2 > 1000){
         texton.style.animation = `fadein 0.3s linear`
         texton.style.opacity = 100+'%'
+
+        mark.forEach(e => {e.style.background="#fafa4588"; e.style.animation='linear ease-in 5s' })
     } 
     if (value * 2 > 2500 || value * 2 < 900 ){
         texton.style.animation = `fadeinSair 0.3s linear`
         texton.style.opacity = 0+'%'
+
     }
 })
