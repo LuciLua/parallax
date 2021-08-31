12eba547bda7e231ad058a072b11dbd782bccdf8
LuciLua <luci.lua81@gmail.com>
2021-08-31 13:25:42 -0300

    :pushpin: overflow hidden

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 let hello = document.getElementById('hello')
-let montain = document.getElementById('montain')
 let texton = document.querySelector('.texton')
 let texto = document.querySelector('.texto')
 let mark= document.querySelectorAll('.mark')
@@ -8,12 +7,12 @@ let retr = document.getElementById('retr')
 window.addEventListener('scroll', function(){
     let value = this.window.scrollY; 
 
-    hello.style.top = 35 + value * 0.02 + '%'
 
-    montain.style.top = `${36 + value * 0.02}%`
-    montain.style.borderRight = `${0 + value * 0.8}px #fafa4588 solid ` 
     retr.style.opacity = 0 + value * 0.05 + '%'
 
+    hello.style.opacity = 100 - value * 0.25 + '%'
+    hello.style.color = `rgb(0, 0, ${value * 0.15})`
+    hello.style.background=`#ff0033${00 + value * 0.05}`
     
     texto.style.top = 50 + value * 0.04 + '%'
     texto.style.opacity = 100 - value * 0.4 + '%'
@@ -22,7 +21,8 @@ window.addEventListener('scroll', function(){
         texton.style.animation = `fadein 0.3s linear`
         texton.style.opacity = 100+'%'
 
-        mark.forEach(e => {e.style.background="#fafa4588"; 
+        mark.forEach(e => {
+        e.style.background="#fafa4588"; 
         e.style.animation='linear ease-in 5s' 
     })
     } 
